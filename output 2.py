d1c43f297274b36e95ab11071b6ed5c2b1a28595
Arjun69101 <96766363+Arjun69101@users.noreply.github.com>
2022-01-21 20:13:36 +0530

    Add files via upload

---
+++ output 2.py
@@ -0,0 +1,34 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============ RESTART: C:/Users/strik/Desktop/python/Assignment 2.py ============
+35
+language sensitive case a is Python
+a case sensitive
+Python is object oriented language
+10
+Python is a case sensitive language
+Enter your name:Arjun
+Enter your SID:21104029
+Enter your department's name:Electrical
+Enter your CGPA:10
+Hey, Arjun here
+My SID is 21104029
+I am from Electrical department and my CGPA is 10
+400
+111610
+111210
+444000
+4040
+27750
+63
+Enter an integer:4
+Enter another integer:8
+Enter a third integer:19
+19
+Enter a sentence:What is your name?
+Yes, it contains the word- name
+First side length:5
+Second side length:6
+Third side length:9
+Yes, a triangle is possible
